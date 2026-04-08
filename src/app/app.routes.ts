d09958788175f4452f66a379ceab1d763d726ffb
Daniel Palacios <palaciosdaniel755@gmail.com>
2026-04-07 22:13:03 -0500

    DFPQ - PRIMER AJSUTE

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { Principal } from '../principal/principal';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: Principal },  //Ruta principal
+    /* {path: 'otra_ruta', component:EjemploComponent} */  //Otra ruta de ejemplo
+];
